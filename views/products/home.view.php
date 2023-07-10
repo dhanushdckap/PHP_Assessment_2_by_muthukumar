8736601458dfe8de8899900606e09bfa251580e2
unknown <dhanushgdckap@gmail.com>
2023-07-10 21:26:44 +0530

    create database input style is going on.

--- views/products/home.view.php
+++ views/products/home.view.php
@@ -1,10 +1,10 @@
 
-<html>
+<html class="h-full bg-gray-100">
 <head>
     <title>home page</title>
     <script src = "https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="h-full">
 <!--
   This example requires updating your template:
 
